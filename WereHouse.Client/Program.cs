6dd4d05436fc2f88259785b3007c73600f7fdf6b
EdwinRey <ereyb@uteq.edu.ec>
2023-12-20 23:07:51 -0500

    Agregar archivos de proyecto.

---
+++ WereHouse.Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+using WereHouse.Client;
+
+var clientWrapper = new WerehouseClienteWrapper("https://localhost:7213");
+
+Console.WriteLine("\n Agregando producto 1...");
+var prodcut1ID = await clientWrapper.AddProductAsync("1", "Coca Cola", 100);
+if (!string.IsNullOrEmpty(prodcut1ID))
+{
+    Console.WriteLine($"Nuevo producto agregado con ID: {prodcut1ID}");
+}
+else
+{
+    Console.WriteLine("No se puedo agregar el producto");
+}
+
+Console.WriteLine("\n Agregando producto 2...");
+var prodcut2ID = await clientWrapper.AddProductAsync("2", "Doritos", 50);
+if (!string.IsNullOrEmpty(prodcut2ID))
+{
+    Console.WriteLine($"Nuevo producto agregado con ID: {prodcut2ID}");
+}
+else
+{
+    Console.WriteLine("No se puedo agregar el producto");
+}
+
+Console.WriteLine("\n Agregando producto 3...");
+var prodcut3ID = await clientWrapper.AddProductAsync("3", "Papa con pollo", 70);
+if (!string.IsNullOrEmpty(prodcut3ID))
+{
+    Console.WriteLine($"Nuevo producto agregado con ID: {prodcut3ID}");
+}
+else
+{
+    Console.WriteLine("No se puedo agregar el producto");
+}
+
+Console.WriteLine("\n Buscando el producto con ID '2'");
+var product = await clientWrapper.GetProductByIdAsync("2");
+if (product!=null)
+{
+    Console.WriteLine($"El producto es: {product.Name}, Cantidad: {product.Cantidad}");
+}
+else
+{
+    Console.WriteLine("No se encontro el producto");
+}
+
+Console.WriteLine("\n Buscando el producto con ID '5'");
+var product5 = await clientWrapper.GetProductByIdAsync("5");
+if (product5 != null)
+{
+    Console.WriteLine($"El producto es: {product5.Name}, Cantidad: {product5.Cantidad}");
+}
+else
+{
+    Console.WriteLine("No se encontro el producto");
+}
+
+Console.WriteLine("\nPreciona una tecla para salir");
+Console.ReadKey();
---
+++ WereHouse.Client/WerehouseClienteWrapper.cs
@@ -0,0 +1,104 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WereHouse.Server;
+
+namespace WereHouse.Client
+{
+    internal class WerehouseClienteWrapper
+    {
+        private readonly Server.WereHouse.WereHouseClient _client;
+
+        public WerehouseClienteWrapper(string address) 
+        { 
+            var channel =  GrpcChannel.ForAddress(address);
+            _client = new Server.WereHouse.WereHouseClient(channel);
+        }
+
+        public async Task<Product> GetProductByIdAsync(string id)
+        {
+            try 
+            {
+                var grpcCall = _client.GetProducttByIdAsync(new ProductID { Id = id });
+                return await CallGrpcServiceAsync(grpcCall);
+            }
+            catch (RpcException)
+            {
+                return null;
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+        }
+
+        public async Task<Product> GetProductByNameAsync(string name)
+        {
+            try
+            {
+                var grpcCall = _client.GetProducttByNameAsync(new ProductName { Name = name });
+                return await CallGrpcServiceAsync(grpcCall);
+            }
+            catch (RpcException)
+            {
+                return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<string> AddProductAsync(string id, string name, int cantidad)
+        {
+            try 
+            {
+                var grpcCall = _client.AddProducttByIdAsync(new Product { Id = id, Name = name, Cantidad = cantidad });
+                var response = await CallGrpcServiceAsync(grpcCall);
+                return response.Id;
+            }
+            catch (RpcException)
+            {
+                return null;
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+        }
+
+        private async Task<T> CallGrpcServiceAsync<T>(AsyncUnaryCall<T> grpcCall)
+        {
+            try
+            {
+                return await grpcCall.ResponseAsync;
+            }
+            catch (RpcException ex)
+            {
+                HandleRpcException(ex);
+                throw;
+            }
+        }
+
+        private void HandleRpcException(RpcException ex)
+        {
+            switch (ex.StatusCode)
+            {
+                case StatusCode.NotFound:
+                    Console.WriteLine($"Error: {ex.Status.Detail}");
+                    break;
+                case StatusCode.Unavailable:
+                    Console.WriteLine("El server esta inactivo. Intenta mas tarde");
+                    break;
+                default:
+                    Console.WriteLine($"Ocurrio un error: {ex.Status.Detail}");
+                    break;
+            }
+        }
+    }
+}
